f66b464a1c67b8892d6c1bf056abd37779430786
RafaelBraido <rafaelbraido126@gmail.com>
2026-03-25 16:40:19 -0300

    ex03 da lista de vetores

---
+++ lista-vetor-01/ex07.js
@@ -0,0 +1,11 @@
+let nums = [10, 12, 120, 2, 90,14];
+let aura = 0;
+
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+        aura++;
+    }
+
+}
+console.log("O total de pares é: ", aura)
+console.log("voce e muito betinha miguel")
