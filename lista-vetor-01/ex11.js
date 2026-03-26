4e7b646f944839a6e41af0da39bd1c04c59edc3e
RafaelBraido <rafaelbraido126@gmail.com>
2026-03-26 14:59:17 -0300

    ex10 ao 14 da lista de vetores

--- lista-vetor-01/ex11.js
+++ lista-vetor-01/ex11.js
@@ -2,7 +2,8 @@ let a = [1, 2, 3, 4, 5]
 let b = []
 
 for (let i = 0 ; i < a.length;i++) {
-    b[i] = a[i] * 2;
+
+b[i] = a[a.length - 1 - i];
 }
 
 console.log(b);
---
+++ lista-vetor-01/ex13.js
@@ -0,0 +1,13 @@
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 9, 8, 7, 6]
+let x = 9
+let aura = 0
+
+for (let i = 0 ; i < a.length;i++) {
+if(a[i]===x){
+aura++
+
+
+
+}
+}
+console.log(aura)
